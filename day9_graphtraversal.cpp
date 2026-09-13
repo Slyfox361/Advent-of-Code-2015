f2f7a64b71082bebc2e2327f3efe9caa3cf9ae0a
Slyfox361 <130494904+Slyfox361@users.noreply.github.com>
2026-09-13 17:10:57 +0100

    Day 9 WITH VECTORS
    i had no idea i was using lists wrong... it was vectors that i wanted and now it all makes sense

--- day9_graphtraversal.cpp
+++ day9_graphtraversal.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
+#include <vector> //this is what is a list in my head. a linked list is actually the <list>. so i've tried using vectors this time :)
 #include <sstream>
 using namespace std;
 
@@ -10,8 +10,8 @@ struct location //this stores information about the places Santa needs to visit
     public:
         string name; //it's name
         //syncronised lists to track its neighbours and the distances to get to them
-        list<location*> neighbours;
-        list<int> distances;
+        vector<location*> neighbours;
+        vector<int> distances;
         location(string n)
         {
             name = n;
@@ -21,49 +21,45 @@ struct location //this stores information about the places Santa needs to visit
 struct returnVal //this is just so my function can return two values
 {
     public:
-        list<location*> sj;
+        vector<location*> sj;
         int ld;
 
-        returnVal(list<location*> l = {nullptr}, int i = 0)
+        returnVal(vector<location*> l = {nullptr}, int i = 0)
         {
             sj = l;
             ld = i;
         }
 };
 
-location* searchList(list<location*> locations, string s) //this is a very simple linear search algorithm so i can search my lists
+location* searchList(vector<location*> locations, string s) //this is a very simple linear search algorithm so i can search my vectors
 {
-    list<location*>::iterator it = locations.begin(); //it uses an iterator to cycle through the list
-    while (it != locations.end())
+    //cycling through the vector
+    for (auto* l : locations)
     {
-        location* l = *it;
         if (l->name == s) //if it finds the item, it returns it
         {
             return l;
         }
-        advance(it,1);
     }
 
     return nullptr; //otherwise it returns a nullptr
 }
 
-returnVal findRoute(location* current, int lowestDist, list<location*> shortestJourney, int currentDist, list<location*> tempJourney) //this is my recursive route searching algorithm (i decided to try and do it from memory instead of searching up an algorithm, so if it is innefficinet or long, that is why! but i am proud i managed to make one regardless)
+returnVal findRoute(location* current, int lowestDist, vector<location*> shortestJourney, int currentDist, vector<location*> tempJourney) //this is my recursive route searching algorithm (i decided to try and do it from memory instead of searching up an algorithm, so if it is innefficinet or long, that is why! but i am proud i managed to make one regardless)
 {
     if (!searchList(tempJourney, current->name)) tempJourney.push_back(current); //just making sure the current node is in the list, otherwise it will skip the start/first node
 
     cout << "Current Node: " << current->name << endl;
-    //iterators to cycle through the neighbours list
-    list<location*>::iterator itN = current->neighbours.begin();
-    list<int>::iterator itD = current->distances.begin();
-    while (itD != current->distances.end())
+    //cycling through the neighbour list
+    for ( int i=0; i < current->neighbours.size(); i++)
     {
-        location* l = *itN; //the current neighbour in focus
+        location* l = current->neighbours[i]; //the current neighbour in focus
         
         if (!searchList(tempJourney, l->name)) //if the neighbour is not in the current route
         {
             int oldDist = currentDist; //store the old dist for backtracking
-            currentDist += *itD; //add to the current dist
-            tempJourney.push_back(*itN); //add to the current route
+            currentDist += current->distances[i]; //add to the current dist
+            tempJourney.push_back(current->neighbours[i]); //add to the current route
             
             returnVal rv = findRoute(l, lowestDist, shortestJourney, currentDist, tempJourney); //moving onto the neighbour to continue the route
 
@@ -76,19 +72,13 @@ returnVal findRoute(location* current, int lowestDist, list<location*> shortestJ
             tempJourney.pop_back();
             currentDist = oldDist;
         }
-        //iterating the iterators
-        advance(itD,1);
-        advance(itN,1);
     }
 
     //debugging
     std::cout << "Current Route :";
-    list<location*>::iterator it = tempJourney.begin();
-    while (it != tempJourney.end())
+    for (auto* l : tempJourney)
     {
-        location* l = *it;
         std::cout << l->name << ", ";
-        advance(it,1);
     }
     cout << endl << currentDist << endl << endl;
 
@@ -111,7 +101,7 @@ int main()
     string line;
 
     //storing the locations
-    list<location*> destinations;
+    vector<location*> destinations;
 
     while (getline(data, line)) //cycling through the file
     {
@@ -151,8 +141,8 @@ int main()
     }
 
     //vars to feed into the findRoute
-    list<location*> l2;
-    list<location*> shortestJourney;
+    vector<location*> l2;
+    vector<location*> shortestJourney;
     returnVal rv;
     int lowestDist = INT_MAX; //set to INT_MAX for part 1 and -1 for part 2
 
@@ -170,12 +160,9 @@ int main()
     
     //printing the shortest route
     std::cout << "Shortest Route :" << endl;
-    list<location*>::iterator it = shortestJourney.begin();
-    while (it != shortestJourney.end())
+    for(auto* l : shortestJourney)
     {
-        location* l = *it;
         std::cout << "{" << l->name << "} " << endl;
-        advance(it,1);
     }
     //and my result
     cout << "Distance: " << lowestDist << endl;
